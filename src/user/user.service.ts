0fa516439491a1d1962cf197636ae1fb389e5493
Joe02exe <lonza.johannes@gmail.com>
2023-07-17 09:35:04 +0200

    added trycatch

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,34 +4,55 @@ import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-    constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
-    async createUser(user : User): Promise<User>  
-    {    
-        console.log("works!")
-        const result = await this.prisma.user.create({data:      
-        user});
-        return result;
+  async createUser(user: User): Promise<User> {
+    try {
+      console.log("works!");
+      const result = await this.prisma.user.create({ data: user });
+      return result;
+    } catch (error) {
+
+        console.error('Error creating user:', error);
+      throw new Error('Failed to create user.');
     }
+  }
 
-    async getAllUsers() {
-        const result = await this.prisma.user.findMany();
-        return result;
+  async getAllUsers() {
+    try {
+      const result = await this.prisma.user.findMany();
+      return result;
+    } catch (error) {
+
+        console.error('Error fetching all users:', error);
+      throw new Error('Failed to fetch users.');
     }
+  }
 
-    async deleteUser(id: string) {
-        const result = await this.prisma.user.delete({
-            where: {username: id}
-        });
-        return result;
-      }
+  async deleteUser(id: string) {
+    try {
+      const result = await this.prisma.user.delete({
+        where: { username: id },
+      });
+      return result;
+    } catch (error) {
 
-    async updateUser(user: User) {
-        const result = await this.prisma.user.update({
-            where: {username: user.username},
-            data: user
-        });
-        return result;
-    }  
-      
+        console.error('Error deleting user:', error);
+      throw new Error('Failed to delete user.');
+    }
+  }
+
+  async updateUser(user: User) {
+    try {
+      const result = await this.prisma.user.update({
+        where: { username: user.username },
+        data: user,
+      });
+      return result;
+    } catch (error) {
+
+        console.error('Error updating user:', error);
+      throw new Error('Failed to update user.');
+    }
+  }
 }
